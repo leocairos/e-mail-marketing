094cefd42866c5fa85e9d1c11795e23432bf1695
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-12-15 10:15:05 -0300

    Contacts log running update

--- backend/contacts-service/src/server.ts
+++ backend/contacts-service/src/server.ts
@@ -10,7 +10,7 @@ import database from 'ms-commons/data/db';
     console.log(`[${process.env.DB_NAME}] Database running...`);
 
     await app.listen(port);
-    console.log(`[Accounts] Service running on port ${port}...`);
+    console.log(`[Contacts] Service running on port ${port}...`);
 
   } catch (error) {
     console.log(`${error}`);
